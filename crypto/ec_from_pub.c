a07d31f9bf5c64b9b620e21e148cd990b510b521
Bourhano <bourhan.dernayka@gmail.com>
2020-11-30 20:56:34 +0000

    remove assignment from if condition (lovely betty)

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -13,10 +13,8 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 
 	if (pub == 0)
 		return (0);
-	if ((key = EC_KEY_new_by_curve_name(EC_CURVE)) == 0)
-		return (0);
-	if ((pt = EC_POINT_new(EC_KEY_get0_group(key))) == 0)
-		return (0);
+	key = EC_KEY_new_by_curve_name(EC_CURVE)) == 0)
+	pt = EC_POINT_new(EC_KEY_get0_group(key))) == 0)
 	if (EC_POINT_oct2point(EC_KEY_get0_group(key),
 			       (EC_POINT *)pt, pub, EC_PUB_LEN, NULL) == 0)
 	{
