03279ac3f33e1cea9d659d68a681782851fbd351
Bourhano <bourhan.dernayka@gmail.com>
2020-12-22 22:03:31 +0000

    implement block difficulty check

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,6 +1,29 @@
 #include "blockchain.h"
 
 /**
+ * get_difficulty - computers number of leading bits in hash
+ * @hash: hash buffer
+ * Return: computed difficulty
+ */
+uint32_t get_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH])
+{
+	uint8_t *ptr = (uint8_t *)hash;
+	uint32_t difficulty = 0;
+	int i;
+
+	for (; ptr < hash + SHA256_DIGEST_LENGTH; ptr++)
+	{
+		for (i = 7; i >= 0; i--)
+		{
+			if ((*ptr >> i) & 1)
+				return (difficulty);
+			difficulty++;
+		}
+	}
+	return (difficulty);
+}
+
+/**
  * hash_matches_difficulty -  checks whether a given hash matches
  * a given difficulty
  * @hash: the hash to check
@@ -11,17 +34,7 @@
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 			uint32_t difficulty)
 {
-	uint8_t i = 0, msb = difficulty & 7;
-
-	difficulty -= msb;
-	difficulty /= 8;
-	while (i < difficulty)
-	{
-		if (hash[i] & 1)
-			return (0);
-		i++;
-	}
-	if (hash[difficulty] >> (8 - msb))
+	if (!hash)
 		return (0);
-	return (1);
+	return (get_difficulty(hash) >= difficulty);
 }
