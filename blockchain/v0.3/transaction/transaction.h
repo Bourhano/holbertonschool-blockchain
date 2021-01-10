6b253f781a6e1193aa87f4f3fdf39467211104a3
Bourhano <bourhan.dernayka@gmail.com>
2021-01-10 15:31:35 +0000

    implement tx validation

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -19,4 +19,23 @@ typedef struct Visitor
 
 } visitor_t;
 
+/**
+ * struct Validation_Visitor - visitor struct for tx validation
+ * @in_amount: total txi amount
+ * @out_amount: total txo amount
+ * @valid: 1 if tx valid else 0
+ * @all_unspent: all unspent txs
+ * @tx: the tx to validate
+ * @block_index: the block index cointaining tx
+ */
+typedef struct Validation_Visitor
+{
+	long in_amount;
+	long out_amount;
+	int valid;
+	llist_t *all_unspent;
+	transaction_t const *tx;
+	uint32_t block_index;
+} validation_vistor_t;
+
 #endif
