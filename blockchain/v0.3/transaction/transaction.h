cd509375c3f6cfa1a27b5670519ce41ec48216ad
Bourhano <bourhan.dernayka@gmail.com>
2021-01-10 15:20:08 +0000

    implement double sided transactions

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -3,4 +3,20 @@
 
 #include "blockchain.h"
 
+/**
+ * struct Visitor - visitor struct for collect sender's unspent
+ * @sender_unspent: list to collect sender's unspent tx
+ * @sender_pub: sender's public key
+ * @total_amount: of unspent tx
+ * @amount: amount to send
+ */
+typedef struct Visitor
+{
+	llist_t *sender_unspent;
+	uint8_t *sender_pub;
+	uint64_t total_amount;
+	uint64_t amount;
+
+} visitor_t;
+
 #endif
